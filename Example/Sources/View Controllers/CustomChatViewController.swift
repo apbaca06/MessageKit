746f0154fe6a9ff8fdfab4fa8e2d8c13a2681e85
jlai <result0924@gmail.com>
2020-03-09 11:22:44 +0800

    Add custom view controller to test MessageKit

---
+++ Example/Sources/View Controllers/CustomChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomChatViewController.swift
+//  ChatExample
+//
+//  Created by justin on 2020/03/09.
+//  Copyright © 2020 MessageKit. All rights reserved.
+//
+
+import UIKit
+
+class CustomChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

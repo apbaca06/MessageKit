fe36314cbd14705241e1b55fd54872ae5aed284e
jlai <result0924@gmail.com>
2020-03-18 17:31:27 +0800

    Add extra button to press

---
+++ Example/Sources/View Controllers/CustomChatViews/ExtraActionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ExtraActionViewController.swift
+//  ChatExample
+//
+//  Created by justin on 2020/03/17.
+//  Copyright © 2020 MessageKit. All rights reserved.
+//
+
+import UIKit
+
+protocol ExtraActionViewControllerDelegate: class {
+    func tapLeft()
+    func tapCenter()
+    func tapRight()
+}
+
+class ExtraActionViewController: UIViewController {
+    weak var delegate: ExtraActionViewControllerDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapLeftButton(_ sender: Any) {
+        self.delegate?.tapLeft()
+    }
+
+    @IBAction func tapCenterButton(_ sender: Any) {
+        self.delegate?.tapCenter()
+    }
+
+    @IBAction func tapRightButton(_ sender: Any) {
+        self.delegate?.tapRight()
+    }
+}
